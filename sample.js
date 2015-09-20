47fadf980543a84e15c40234a7f6d60543af30c9
ctoeniskoetter <claretoeniskoetter@gmail.com>
2015-09-19 20:00:45 -0400

    added two images
    added image at 2 seconds and 15 to 30 seconds

--- sample.js
+++ sample.js
@@ -1,11 +1,33 @@
 (function() {
   // data to be passed into the instance scope
   var sample = {
-    "cuts": ["2", "5", "8", "11", "30", "39", "45"],
     "time_2":{
       "start": "2",
-      "end":"33",
+      "end":"14",
       "data": {
+        "image" : "http://mediad.publicbroadcasting.net/p/kalw/files/99invisible-logo-itunes-badge.jpg",
+        "actions" : [
+          {
+            "type":"youtube",
+            "url": "youtube.com/url"
+          },
+          {
+            "type":"twitter",
+            "url":"twitter.com/url/tweet"
+          }
+        ],
+        "functions":[
+            {
+              "function_name":"name"
+            }
+        ]
+      }
+    },
+       "time_2":{
+      "start": "15",
+      "end":"30",
+      "data": {
+        "image" : "http://i2.wp.com/99percentinvisible.org/wp-content/uploads/2015/08/8712925526_bc94d59c61_h.jpg",
         "actions" : [
           {
             "type":"youtube",
