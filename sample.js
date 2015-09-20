908a8167d1f937ee6d0e403c6221aa84f8b22331
ctoeniskoetter <claretoeniskoetter@gmail.com>
2015-09-19 20:26:49 -0400

    new image at 5 seconds

--- sample.js
+++ sample.js
@@ -3,9 +3,31 @@
   var sample = {
     "time_2":{
       "start": "2",
-      "end":"14",
+      "end":"5",
       "data": {
         "image" : "http://mediad.publicbroadcasting.net/p/kalw/files/99invisible-logo-itunes-badge.jpg",
+        "actions" : [
+          {
+            "type":"youtube",
+            "url": "youtube.com/url"
+          },
+          {
+            "type":"twitter",
+            "url":"twitter.com/url/tweet"
+          }
+        ],
+        "functions":[
+            {
+              "function_name":"name"
+            }
+        ]
+      }
+    },
+    "time_5":{
+      "start": "5",
+      "end":"14",
+      "data": {
+        "image" : "http://i2.wp.com/99percentinvisible.org/wp-content/uploads/2015/08/Screen-Shot-2015-08-10-at-12.05.10-PM.png",
         "actions" : [
           {
             "type":"youtube",
