45293ed150f01954f0f592a954c3e634e6cf802c
Vijith Assar <vijithassar@yahoo.com>
2015-09-20 09:58:38 -0400

    clearer spacing

--- demo.js
+++ demo.js
@@ -20,23 +20,28 @@
   };
 
   get_json('/static/sample.json', function(results) {
+
     // select audio node based on target string
     var id = 'target';
     var sample_audio = document.getElementById(id);
+
     // instantiate
     var smart_podcast = extaudio()
       // set audio node
       .node(sample_audio)
       // set data
       .all_data(results);
+
     // bind a function so it has access to the scoped data via arguments
     smart_podcast.extend('test', function(data, timestamp, node) {
       console.log('bound', data, timestamp, node)
     });
+
     // run the bound function on every update
     smart_podcast.tick(function(data, timestamp, node) {
       smart_podcast.test();
     });
+
     // execute instance
     smart_podcast();
 
